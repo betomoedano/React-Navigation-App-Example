474eba215ea69e98148c3a61720eec06628afbba
betomoedano <betomoedano@outlook.com>
2023-05-27 10:04:34 -0400

    add home stack

--- Navigation.js
+++ Navigation.js
@@ -1,11 +1,68 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Feed from "./screens/tabScreens/Feed";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Settings from "./screens/tabScreens/Settings";
+import Notifications from "./screens/tabScreens/Notifications";
+import { Ionicons } from "@expo/vector-icons";
+import TweetDetailScreen from "./screens/homeStack/TweetDetailsScreen";
+
+// Stack
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackGroup() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="TabsGroup" component={TabsGroup} />
+      <HomeStack.Screen
+        name="TweetDetailScreen"
+        component={TweetDetailScreen}
+        options={{ presentation: "modal", headerTitle: "Tweet Details" }}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+// Tabs
+const Tab = createBottomTabNavigator();
+
+function TabsGroup() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        // headerTitleAlign: "center",
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "ios-settings-sharp";
+          } else if (route.name === "Notifications") {
+            iconName = focused ? "ios-notifications" : "notifications-outline";
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#1DA1F2",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{ tabBarLabel: "Feed", headerShown: false }}
+      />
+      <Tab.Screen name="Notifications" component={Notifications} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Feed />
+      <HomeStackGroup />
     </NavigationContainer>
   );
 }
---
+++ screens/homeStack/TweetDetailsScreen.js
@@ -0,0 +1,26 @@
+import { useRoute } from "@react-navigation/native";
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+
+import TweetContent from "../../components/TweetContent";
+
+const TweetDetailScreen = () => {
+  const {
+    params: { tweet },
+  } = useRoute();
+  return (
+    <View testID="TweetDetailScreen" style={styles.container}>
+      <StatusBar barStyle={"light-content"} />
+      <TweetContent tweet={tweet} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
+
+export default TweetDetailScreen;
